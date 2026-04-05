cbfce5701fdaf66450a3d730d3ee598d39ce4181
Mahek Imtiyaz <maherimtiyaz7@gmail.com>
2026-04-05 18:23:31 +0530

    Added workers

--- app/api/ai_routes.py
+++ app/api/ai_routes.py
@@ -3,6 +3,15 @@ from app.utils.embeddings import get_embedding
 from app.utils.faiss_store import search
 from openai import OpenAI
 import os
+from fastapi import APIRouter
+from app.services.ai_service import answer_query
+
+router = APIRouter()
+
+@router.post("/query")
+def query(q: str):
+    return answer_query(q)
+
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
