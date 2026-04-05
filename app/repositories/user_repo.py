2b1ea9f2db58be74494b8305ef7d0105dc56f6d3
Mahek Imtiyaz <maherimtiyaz7@gmail.com>
2026-04-05 18:24:46 +0530

    Added user_repo in repo

---
+++ app/repositories/user_repo.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+
+def create_user(db: Session, email: str, password: str):
+    user = User(email=email, password=password)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def get_user_by_id(db: Session, user_id: str):
+    return db.query(User).filter(User.id == user_id).first()
