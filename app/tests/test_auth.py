fded0b413bd0d89f2b7b77875d643363c74f36ee
Mahek Imtiyaz <maherimtiyaz7@gmail.com>
2026-04-05 18:30:10 +0530

    added integration tests using FastAPI TestClient to validate API endpoints.

---
+++ app/tests/test_auth.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_register():
+    response = client.post("/auth/register", json={
+        "email": "test@test.com",
+        "password": "1234"
+    })
+    assert response.status_code == 200
+
+def test_login():
+    response = client.post("/auth/login", json={
+        "email": "test@test.com",
+        "password": "1234"
+    })
+    assert response.status_code == 200
+    assert "access_token" in response.json()
